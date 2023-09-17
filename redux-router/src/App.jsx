2056aad3d8d8bb5cd805458026aeee2c12d5f2be
aa-r-ju <aarju3447@gmail.com>
2023-09-17 20:41:37 +0545

    feat:completed part7 video b

--- redux-router/src/App.jsx
+++ redux-router/src/App.jsx
@@ -1,11 +1,26 @@
 import {BrowserRouter as Router,Routes, Route, Link} from 'react-router-dom'
+import Notes from './Notes'
+import Note from './Note'
+const notes = [
+  {
+    "content": "the app state is in redux store",
+    "important": true,
+    "id": 1
+  },
+  {
+    "content": "state changes are made with actions",
+    "important": false,
+    "id": 2
+  },
+  {
+    "content": "one more",
+    "important": true,
+    "id": 3
+  }
+]
 
 const Home = () => (
   <div> <h2>TKTL notes app</h2> </div>
-)
-
-const Notes = () => (
-  <div> <h2>Notes</h2> </div>
 )
 
 const Users = () => (
@@ -27,7 +42,8 @@ const App = () => {
       </div>
 
       <Routes>
-        <Route path="/notes" element={<Notes />} />
+      <Route path="/notes/:id" element={<Note notes={notes} />} />
+        <Route path="/notes" element={<Notes notes = {notes}/>} />
         <Route path="/users" element={<Users />} />
         <Route path="/" element={<Home />} />
       </Routes>
