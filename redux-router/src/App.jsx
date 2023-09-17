9918b2db53c58945f300c7e97f3e759b94978910
aa-r-ju <aarju3447@gmail.com>
2023-09-17 21:55:05 +0545

    feat:completed part7 workshop video d

--- redux-router/src/App.jsx
+++ redux-router/src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router,Routes, Route, Link,Navigate} from 'react-router-dom'
+import {Routes, Route, Link,Navigate,useMatch} from 'react-router-dom'
 import Notes from './Notes'
 import Note from './Note'
 import { useState } from 'react'
@@ -32,12 +32,18 @@ const Users = () => (
 const App = () => {
   const[user, setUser] = useState(null)
 
+  const match = useMatch('/notes/:id')
+  const note = match 
+    ? notes.find(note => note.id === Number(match.params.id))
+    : null
+
+
   const padding = {
     padding: 5
   }
 
   return (
-    <Router>
+    <>
       <div>
         <Link style={padding} to="/">home</Link>
         <Link style={padding} to="/notes">notes</Link>
@@ -49,7 +55,7 @@ const App = () => {
       </div>
 
       <Routes>
-      <Route path="/notes/:id" element={<Note notes={notes} />} />
+      <Route path="/notes/:id" element={<Note note={note} />} />
         <Route path="/notes" element={<Notes notes = {notes}/>} />
         <Route path="/users" element={<Users />} />
         <Route path="/users" element={user ? <Users /> : <Navigate replace to="/login" />} />
@@ -60,7 +66,7 @@ const App = () => {
       <div>
         <i>Note app, Department of Computer Science 2023</i>
       </div>
-    </Router>
+    </>
   )
 }
 export default App
